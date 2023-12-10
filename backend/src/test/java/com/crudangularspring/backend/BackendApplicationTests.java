2cba5fdf68564fdb3fddf882512c078ac8924c47
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2023-12-10 12:16:19 -0300

    feat: initialize backend repo

---
+++ backend/src/test/java/com/crudangularspring/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudangularspring.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
