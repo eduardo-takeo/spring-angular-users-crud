dd9d42634ca9b4455751eae11671d95e01cd34b3
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2023-12-21 21:22:49 -0300

    feat: create user form route and page

--- frontend/src/app/modules/users/users.routing.module.ts
+++ frontend/src/app/modules/users/users.routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users.component';
+import { UsersDetailsComponent } from './pages/users-details/users-details.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UsersComponent,
+  },
+  {
+    path: ':id',
+    component: UsersDetailsComponent,
   },
 ];
 
